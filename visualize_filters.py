fd4aabf033c95cbb562dc0aa1fa51a401c97bb32
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-02-17 17:29:34 -0800

    Added compute metrics and filter visualization code

---
+++ visualize_filters.py
@@ -0,0 +1,25 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from baseline_cnn import BasicCNN
+import sys
+
+
+def visualize_weights(weights):
+    n_cols = 3
+    n_rows = np.ceil(len(weights)/n_cols)
+    print(n_rows,n_cols)
+    plt.figure(figsize = [3.2,2.4])
+    weights = weights.detach().numpy()
+    for i in range(len(weights)):
+        plt.subplot(n_rows,n_cols,i+1)
+        plt.imshow(weights[i][0])
+
+    plt.show()
+
+
+
+model= BasicCNN()
+model.load_state_dict(torch.load(sys.argv[1]))
+
+visualize_weights(model.conv1.weight)

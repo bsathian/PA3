466d0d6c69e66951e1c3621d47d07b394e0eb6b5
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-02-18 15:28:42 -0800

    Update compute metrics

--- compute_metrics.py
+++ compute_metrics.py
@@ -26,7 +26,7 @@ for i in range(15):
 #Write out a LaTeX worthy confusion matrix
 cmPercent =  cm/np.sum(cm,axis = 0) * 100
 
-cmTable = open("cmTable_baseline.txt","w")
+cmTable = open("cmTable_arch2.txt","w+")
 
 cmTable.write("\\begin{table}\n")
 cmTable.write("\centering\n")
@@ -44,6 +44,11 @@ for i in range(len(cmPercent)):
     cmTable.write("\\hline\n")
 cmTable.write("\\end{tabular}\n\\end{table}")
 
+weights = np.array([0.42629763, 0.08048455, 0.02015054, 0.09204955, 0.14309236,0.03908578, 0.04571115, 0.00956563, 0.0381449 , 0.03340129,0.01748471, 0.01913125, 0.01148659, 0.02234593, 0.00156814])
+
+print("Total precision=",np.sum(weights * precision))
+print("Total recall=",np.sum(weights * recall))
+print("Total BCR=",np.sum(weights * 0.5 * (precision + recall)))
 #plt.imshow(cmPercent, cmap = "gray")
 #plt.colorbar()
 #plt.show()
